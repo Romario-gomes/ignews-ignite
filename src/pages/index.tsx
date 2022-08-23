5162a8c4708c8fa41a78f7bb1b001294abbca71d
Romario Alves <romariogn10@gmail.com>
2022-08-23 07:42:24 -0300

    Feat(stripe): configurando e consumindo api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,23 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import { stripe } from '../components/services/stripe';
 import { SubscribeButton } from '../components/SubscribeButton';
 import styles from './home.module.scss'; 
 
-export default function Home() {
+
+//chamadas api
+// client-side
+// server-side
+// static site generation
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -14,12 +29,31 @@ export default function Home() {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for { product.amount } month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/mulher.svg" alt="Girl coding" />
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1LZte2FBiWdxhePBFqGPOqXD')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, //24 hours
+  }
 }
