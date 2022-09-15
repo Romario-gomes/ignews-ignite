c376c6bee8508c3f6b278b5e68d85c52520659d8
Romario Alves <romariogn10@gmail.com>
2022-09-14 21:27:01 -0300

    Feat(Router): add active link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,23 @@
 import { SignInButton } from '../SignInButton';
+import Link from 'next/link'
 import styles from './styles.module.scss'; 
+import { useRouter } from 'next/router';
+import { ActiveLink } from '../ActiveLink';
 
 export function Header() {
+  const { asPath } = useRouter();
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active} href="">Home</a>
-          <a href="">Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a >Home</a>
+          </ActiveLink>
+          <ActiveLink href="/posts" activeClassName={styles.active}>
+            <a >Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
